4af1ab9ece665bc744917b03f024530dff3ad678
Kacey Cleveland <kleveland@github.com>
2022-12-12 22:25:14 -0500

    Update open graph image function

--- pages/api/posts/og.tsx
+++ pages/api/posts/og.tsx
@@ -1,38 +1,33 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import chromium from "chrome-aws-lambda";
-import { chromium as playwrightChromium } from "playwright-core";
 import { getAbsoluteURL } from "utils/utils";
+import chrome from "chrome-aws-lambda";
+import puppeteer from "puppeteer-core";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  // Start the browser with the AWS Lambda wrapper (chrome-aws-lambda)
-  const browser = await playwrightChromium.launch(
-    process.env.IS_LOCAL
-      ? undefined
-      : {
-          args: chromium.args,
-          executablePath: await chromium.executablePath,
-          headless: chromium.headless,
-        }
-  );
-
-  // Create a page with the Open Graph image size best practice
-  const page = await browser.newPage({
-    viewport: {
-      width: 1200,
-      height: 630,
-    },
-  });
-  // Generate the full URL out of the given path (GET parameter)
+  const options = process.env.AWS_REGION
+    ? {
+        args: chrome.args,
+        executablePath: await chrome.executablePath,
+        headless: chrome.headless,
+      }
+    : {
+        args: [],
+        executablePath:
+          process.platform === "win32"
+            ? "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
+            : process.platform === "linux"
+            ? "/usr/bin/google-chrome"
+            : "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
+      };
+  const browser = await puppeteer.launch(options);
+  const page = await browser.newPage();
+  await page.setViewport({ width: 1200, height: 630 });
   const relativeUrl = (req.query["path"] as string) || "";
   const url = getAbsoluteURL(relativeUrl);
-
-  await page.goto(url, {
-    timeout: 30 * 1000,
-    waitUntil: "networkidle",
-  });
+  await page.goto(url, { waitUntil: "networkidle0" });
   const data = await page.screenshot({
     type: "png",
   });
