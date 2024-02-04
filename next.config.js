0b1bc7a7b5783b78408c74042bdfc4024a2b2b35
Kacey Cleveland <kacey@arrivedhomes.com>
2024-02-04 12:51:14 -0500

    fix: address any nextjs-isms

--- next.config.js
+++ next.config.js
@@ -1,19 +1,28 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: [
-      "images.ctfassets.net",
-      "www.gravatar.com",
-      "www.kleveland.dev",
-      "cdn.sanity.io",
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.ctfassets.net',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.gravatar.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.kleveland.dev',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.sanity.io',
+      },
     ],
   },
   productionBrowserSourceMaps: true,
   reactStrictMode: true,
   swcMinify: true,
-  experimental: {
-    appDir: true,
-  },
 };
 
 module.exports = nextConfig;
